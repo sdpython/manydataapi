75362663878e31a3ef9b2a4f70439d9e7729f6aa
xavier dupré <xavier.dupre@gmail.com>
2019-04-06 15:18:05 +0200

    documentation theme

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-import sphinxjp.themes.basicstrap
+import sphinx_readable_theme
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 
 set_sphinx_variables(__file__, "manydataapi", "Xavier Dupré", 2019,
-                     "basicstrap", None, locals(), add_extensions=None,
+                     "readable", sphinx_readable_theme.get_html_theme_path(),
+                     locals(), add_extensions=None,
                      extlinks=dict(issue=('https://github.com/sdpython/manydataapi/issues/%s', 'issue')))
 
 blog_root = "http://www.xavierdupre.fr/app/manydataapi/helpsphinx/"
