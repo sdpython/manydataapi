e90ba2f7e0bdc6c54e54ff9924626d645a4b57ab
xavier dupré <xavier.dupre@gmail.com>
2021-01-01 04:01:53 +0100

    update to python 3.9

--- src/manydataapi/__init__.py
+++ src/manydataapi/__init__.py
@@ -8,7 +8,7 @@ mostly used for teaching purpose.
 import sys
 
 
-__version__ = "0.2"
+__version__ = "0.2.77"
 __author__ = "Xavier Dupré"
 __github__ = "https://github.com/sdpython/manydataapi"
 __url__ = "http://www.xavierdupre.fr/app/manydataapi/helpsphinx/index.html"
