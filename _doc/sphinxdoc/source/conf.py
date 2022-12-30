aa2b70564464d81232cb6abb48a464e0b29de522
xavier dupré <xavier.dupre@gmail.com>
2022-12-30 23:33:13 +0100

    Update conf.py

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -9,7 +9,9 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 set_sphinx_variables(__file__, "manydataapi", "Xavier Dupré", 2021,
                      "alabaster", alabaster.get_path(),
                      locals(), add_extensions=None,
-                     extlinks=dict(issue=('https://github.com/sdpython/manydataapi/issues/%s', 'issue')))
+                     extlinks=dict(issue=(
+                         'https://github.com/sdpython/manydataapi/issues/%s',
+                         'issue %s')))
 
 blog_root = "http://www.xavierdupre.fr/app/manydataapi/helpsphinx/"
 blog_background = False
